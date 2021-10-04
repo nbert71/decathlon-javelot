95943ee415903d62d47b7ac3a69513f43957380c
Nicolas <nicobert2205@gmail.com>
2021-10-04 21:45:35 +0200

    refactoring

--- javelot.js
+++ javelot.js
@@ -6,54 +6,54 @@ function lancerDe() {
   return Math.floor(6 * Math.random() + 1);
 }
 
+function getDice(i) {
+  return document.querySelector("#dice" + (i + 1)).textContent
+}
+
+// function getDices() {
+//   liste_des = [];
+//   for (let i = 0; i < nombreDes; i++) {
+//     liste_des[i] = document.querySelector("#dice" + (i + 1)).textContent;
+//   }
+//   return liste_des
+// }
+
 function lancerDes() {
   for (let i = 0; i < nombreDes; i++) {
     if (isTicked(i)) {
       disableCheckbox(i);
     } else document.querySelector("#dice" + (i + 1)).textContent = lancerDe()
   }
-  score += getSumTickedNumber();
+  score += getSumValidTickedNumber();
   majScore()
 }
 
-function calculScore() {
-  liste_des = getDices();
-  let score = 0;
-  for (let index = 0; index < liste_des.length; index++) {
-    if (liste_des[i] % 2 != 0) {
-      // le nombre est impair
-      score += liste_des[i];
-    }
-  }
-  return score;
-}
+// function calculScore() {
+//   liste_des = getDices();
+//   let score = 0;
+//   for (let index = 0; index < liste_des.length; index++) {
+//     if (liste_des[i] % 2 != 0) {
+//       // le nombre est impair
+//       score += liste_des[i];
+//     }
+//   }
+//   return score;
+// }
 
 document.querySelector("#roll_dices").addEventListener("click", (event) => {
   lancerDes()
   event.preventDefault();
 })
 
-function getDice(i) {
-  return document.querySelector("#dice" + (i + 1)).textContent
-}
-
-function getDices() {
-  liste_des = [];
-  for (let i = 0; i < nombreDes; i++) {
-    liste_des[i] = document.querySelector("#dice" + (i + 1)).textContent;
-  }
-  return liste_des
-}
-
-function getCheckboxValue() {
-  Ticked = []
-  for (let i = 0; i < nombreDes; i++) {
-    if (isTicked(i)) {
-      Ticked[i] = true;
-    } else Ticked[i] = false;
-  }
-  return Ticked
-}
+// function getCheckboxValue() {
+//   Ticked = []
+//   for (let i = 0; i < nombreDes; i++) {
+//     if (isTicked(i)) {
+//       Ticked[i] = true;
+//     } else Ticked[i] = false;
+//   }
+//   return Ticked
+// }
 
 function isTicked(i) {
   return document.querySelector("#tick" + (i + 1)).checked
@@ -65,7 +65,7 @@ function disableCheckbox(i) {
   }
 }
 
-function getSumTickedNumber() {
+function getSumValidTickedNumber() {
   let somme = 0
   for (let i = 0; i < nombreDes; i++) {
     if (isTicked(i) && parseInt(getDice(i)) % 2 != 0) {
