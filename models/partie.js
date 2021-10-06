af9c104ce3b797d28eb8d404e163ca5f8862167e
Nicolas <nicobert2205@gmail.com>
2021-10-06 09:52:55 +0200

    ajout schema db

--- models/partie.js
+++ models/partie.js
@@ -1,1 +1,22 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Partie', {
+        pseudo: {
+            type: DataTypes.STRING,
+            allownull: false
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allownull: false
+        },
+        des: {
+            type: DataTypes.JSON,
+            allownull: true
+        },
+        compteur :{
+            type: DataTypes.SMALLINT,
+            allownull: false
+        }
+    })
+}
