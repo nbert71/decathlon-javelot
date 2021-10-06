c67371006593ebd1f993e5c1c14192ca6d951b31
royantk <killianr479@gmail.com>
2021-10-06 19:31:11 +0200

    ajout du score en temps réel, d'une condition terminant la partie et affichant un gameover, d'une condition empêchant de lancer un dé si il n'y a pas de dé pair coché, d'un évènement débutant la partie quand la page est chargée et de commentaires expliquant les fonctions

--- javelot.js
+++ javelot.js
@@ -1,23 +1,31 @@
+/*
+VARIABLES
+*/
+
 var nombreDes = 6;
 var score = 0;
+var cpt = 1;
+var game_started = false;
 
+
+
+/*
+FONCTIONS
+*/
+
+// Fonction qui renvoie un entier aléatoire entre 1 et 6
 function lancerDe() {
   // random --> [0,1[
   return Math.floor(6 * Math.random() + 1);
 }
 
+// Fonction qui récupère la valeur du dé i
 function getDice(i) {
   return document.querySelector("#dice" + (i + 1)).textContent
 }
 
-// function getDices() {
-//   liste_des = [];
-//   for (let i = 0; i < nombreDes; i++) {
-//     liste_des[i] = document.querySelector("#dice" + (i + 1)).textContent;
-//   }
-//   return liste_des
-// }
-
+// Fonction qui lance tous les dés non-cochés, désactive les dés cochés 
+// et met à jour le score
 function lancerDes() {
   for (let i = 0; i < nombreDes; i++) {
     if (isTicked(i)) {
@@ -25,8 +33,128 @@ function lancerDes() {
     } else document.querySelector("#dice" + (i + 1)).textContent = lancerDe()
   }
   score = getSumValidTickedNumber();
-  majScore()
+  isGameOver ();
 }
+
+// Fonction qui vérifie si le dé i est coché
+function isTicked(i) {
+  return document.querySelector("#tick" + (i + 1)).checked
+}
+
+// Fonction qui vérifie si le dé i est actif
+function isActive(i) {
+  return !document.querySelector("#tick" + (i + 1)).disabled
+}
+
+// Fonction qui désactive le dé i s'il est coché
+function disableCheckbox(i) {
+  document.querySelector("#tick" + (i + 1)).disabled = true;
+}
+
+// Fonction qui calcule la somme des valeurs des dés impairs cochés (score actuel)
+function getSumValidTickedNumber() {
+  let somme = 0;
+  for (let i = 0; i < nombreDes; i++) {
+    if (isTicked(i) && parseInt(getDice(i)) % 2 != 0) {
+      value = parseInt(getDice(i));
+      somme += value;
+    }
+  }
+  return somme;
+}
+
+// Fonction qui met à jour le score et l'affichage du bouton de lancer
+function majScore () {
+  document.querySelector("#score").textContent = score;
+  document.querySelector("#roll_dices").style.visibility = 'hidden';
+  if (isAnyEnabledDiceTicked ()) document.querySelector("#roll_dices").style.visibility = 'visible'
+}
+
+// Fonction qui vérifie s'il existe un dé impair actif est coché
+function isAnyEnabledDiceTicked () {
+  for (let i = 0; i < nombreDes; i++) {
+    if (isTicked(i) && isActive(i) && parseInt(getDice(i)) % 2 != 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Fonction qui vérifie s'il reste des dés disponibles
+function isAnyAvaliableDice () {
+  for (let i = 0; i < nombreDes; i++) {
+    if (isActive(i) && parseInt(getDice(i)) % 2 != 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Fonction qui vérifie si la partie peut continuer et qui l'arrête sinon
+function isGameOver () {
+  if (game_started && (cpt > 3 || !isAnyAvaliableDice ())) {
+    for (let i = 0; i < nombreDes; i++) {
+      disableCheckbox(i);
+    }
+    document.querySelector("#gameover").style.visibility = 'visible';
+    document.querySelector("#replay").style.visibility = 'visible';
+  }
+}
+
+/*
+LISTENERS
+*/
+
+// Listener bouton "nouvelle partie" (lance les dés et met à jour le score)
+document.addEventListener("DOMContentLoaded", (event) => {
+  lancerDes();
+  game_started = true;
+  event.preventDefault();
+  document.querySelector("#roll_dices").style.visibility = 'hidden';
+  cpt += 1;
+});
+
+// Listener bouton "lancer de dés" (lance les dés et met à jour le score)
+document.querySelector("#roll_dices").addEventListener("click", (event) => {
+  lancerDes();
+  game_started = true;
+  event.preventDefault();
+  document.querySelector("#roll_dices").style.visibility = 'hidden';
+  cpt += 1;
+  
+})
+
+// Listener clic sur les checkbox (met à jour le score en direct
+// et affiche le bouton lancer si un dés impair actif est coché)
+document.querySelector("#tick1").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+document.querySelector("#tick2").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+document.querySelector("#tick3").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+document.querySelector("#tick4").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+document.querySelector("#tick5").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+document.querySelector("#tick6").addEventListener("click", (event) => {
+  score = getSumValidTickedNumber();
+  majScore();
+})
+
+
+/*
+FONCTIONS DESACTIVEES
+*/
 
 // function calculScore() {
 //   liste_des = getDices();
@@ -40,11 +168,6 @@ function lancerDes() {
 //   return score;
 // }
 
-document.querySelector("#roll_dices").addEventListener("click", (event) => {
-  lancerDes()
-  event.preventDefault();
-})
-
 // function getCheckboxValue() {
 //   Ticked = []
 //   for (let i = 0; i < nombreDes; i++) {
@@ -55,27 +178,11 @@ document.querySelector("#roll_dices").addEventListener("click", (event) => {
 //   return Ticked
 // }
 
-function isTicked(i) {
-  return document.querySelector("#tick" + (i + 1)).checked
-}
 
-function disableCheckbox(i) {
-  if (isTicked(i)) {
-    document.querySelector("#tick" + (i + 1)).disabled = true
-  }
-}
-
-function getSumValidTickedNumber() {
-  let somme = 0
-  for (let i = 0; i < nombreDes; i++) {
-    if (isTicked(i) && parseInt(getDice(i)) % 2 != 0) {
-      value = parseInt(getDice(i));
-      somme += value;
-    }
-  }
-  return somme
-}
-
-function majScore () {
-  document.querySelector("#score").textContent = score
-}
+// function getDices() {
+//   liste_des = [];
+//   for (let i = 0; i < nombreDes; i++) {
+//     liste_des[i] = document.querySelector("#dice" + (i + 1)).textContent;
+//   }
+//   return liste_des
+// }
